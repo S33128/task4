fd9b1e784cb9067bd9de0e1625e34f2b50902afc
s33128 <s33128@pjwstk.edu.pl>
2025-04-16 21:00:13 +0200

    klasa Sklep Komputerowy

---
+++ zadanie4/src/SklepKomputerowy.java
@@ -0,0 +1,16 @@
+public class SklepKomputerowy {
+    private Produkt[] produkty;
+    private Klient[] klienci;
+    private Zamowienie[] zamowienia;
+    private int liczbaProdukt;
+    private int liczbaKlient;
+    private int liczbaZamowienia;
+
+    public void dodajProdukt(Produkt produkt) {}
+    public void dodajKlient(Klient klient) {}
+    public void utworzZamowienie(Klient klient, Produkt[] produkt, Zamowienie[] zamowienia) {}
+    public void aktualizujStanMagazynu(Zamowienie zamowienie) {}
+    public void zmienStatusZamowienia(int idZamowienia, String nowyStatus) {}
+    public void wyswietlProuktyWKategori(String kategoria){}
+    public void wyswieltZamowieniaKlienta(int idKlienta){}
+}
